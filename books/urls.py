01cad5f24f0c31face8a606cee66040bb20eee98
Dady <ddady1@gmail.com>
2024-06-06 22:24:40 +0300

    working html

--- books/urls.py
+++ books/urls.py
@@ -6,7 +6,7 @@ from books import views
 
 
 urlpatterns = [
-    #path('', views.book_index, name='book_index'),
+    path('books/', views.book_index, name='book_index'),
     path('', views.home_html, name='home_html'),
     #path('<int:pk>/', views.book_detail, name='book_detail'),
 ]
