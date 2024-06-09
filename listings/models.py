81eb67a550ef0d43f374f59dc570d9a9cf3796c2
Dady <ddady1@gmail.com>
2024-06-09 22:22:05 +0300

    listings

--- listings/models.py
+++ listings/models.py
@@ -5,5 +5,5 @@ from django.db import models
 class Band(models.Model):
     name = models.fields.CharField(max_length=100)
 
-class listing(models.Model):
+class Listing(models.Model):
     title = models.fields.CharField(max_length=100)
--- listings/views.py
+++ listings/views.py
@@ -3,23 +3,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from listings.models import Band
+from listings.models import Listing
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f'''<h1>Hello Bitch!</h1>
-                        <p>My favorite bands are:<p>
-                        <ul>
-                            <li>{bands[0].name}<li>
-                            <li>{bands[1].name}<li>
-                            <li>{bands[2].name}<li>
-                        </ul>
-                        ''')
+    return render(request, 'listings/hello.html', {'bands': bands})
 
 def about(request):
     return HttpResponse('<h1>About Us</h1> <p>We love Python</p>')
 
 def listings(request):
-    return HttpResponse('<h1>This is LISTINGS page, BITCH!!</h1>')
+    titles = Listing.objects.all()
+    return HttpResponse(f'''<h1>This is LISTINGS page, BITCH!!</h1>
+                        <p>My best song:</p>
+                        <ul>
+                            <li>{titles[0].title}</li>
+                            <li>{titles[1].title}</li>
+                            <li>{titles[2].title}</li>
+                            <li>{titles[3].title}</li>
+                        ''')
 
 def contact(request):
     return HttpResponse('<h2>Dont call us we"ll call you CONTACT page</h2>')
