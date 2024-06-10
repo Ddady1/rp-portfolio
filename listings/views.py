2eb5121a5db52cdd9d8125268d6174e1d8424b91
Dady <ddady1@gmail.com>
2024-06-10 21:37:22 +0300

    chapter 5

--- listings/views.py
+++ listings/views.py
@@ -10,20 +10,13 @@ def hello(request):
     return render(request, 'listings/hello.html', {'bands': bands})
 
 def about(request):
-    return HttpResponse('<h1>About Us</h1> <p>We love Python</p>')
+    return render(request, 'listings/about.html')
 
 def listings(request):
     titles = Listing.objects.all()
-    return HttpResponse(f'''<h1>This is LISTINGS page, BITCH!!</h1>
-                        <p>My best song:</p>
-                        <ul>
-                            <li>{titles[0].title}</li>
-                            <li>{titles[1].title}</li>
-                            <li>{titles[2].title}</li>
-                            <li>{titles[3].title}</li>
-                        ''')
+    return render(request, 'listings/listings.html', {'titles': titles})
 
 def contact(request):
-    return HttpResponse('<h2>Dont call us we"ll call you CONTACT page</h2>')
+    return render(request, 'listings/contact.html')
 
 # Create your views here.
