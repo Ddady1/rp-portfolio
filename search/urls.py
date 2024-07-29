8c7f389b779787f69a21494d48f5e8257f5a6fec
Ddady1 <ddady1@gmail.com>
2024-07-29 15:56:11 +0300

    start search project added model, url, style and view added urls.py file under search project and search_main.html file added base.html Added a model Edited search_main.html Added Tzomet script file and test.html file and edited view getting now no reverse error can get view with items. need to fix code of tzomet. checked stimzaky script which works good now displaying both tzomet and stimazky in results

--- search/urls.py
+++ search/urls.py
@@ -7,4 +7,5 @@ from search import views
 urlpatterns = [
     path('search/', views.search_main, name='search-main'),
     path('results/', views.search_results, name='search-results'),
+    #path('results/<dict:book_details>/', views.search_results, name='search-results'),##1
 ]
--- search/views.py
+++ search/views.py
@@ -9,13 +9,14 @@ from search.Stimatzky_script import stimazky
 def search_main(request):
     if request.method == 'POST':
         book_name = request.POST.get('book_name')
-        #results = tzomet(book_name)
-        results = stimazky(book_name)
+        tzomet_results = tzomet(book_name)
+        stimazky_results = stimazky(book_name)
         #results = 'good book'
-        print(results)
+        #print(results)
         #return HttpResponse(f'{results}')
-        request.session['results'] = results
-        return redirect('search-results')
+        request.session['tzomet_results'] = tzomet_results ##1
+        request.session['stimazky_results'] = stimazky_results ##1
+        return redirect('search-results') ##1
         #return redirect('search-results', results)
         #return search_results(request, results) stays on the same page
     else:
@@ -23,8 +24,9 @@ def search_main(request):
 
 #def search_results(request, book_details):
 def search_results(request):
-    book_details = request.session.get('results')
-    return render(request, 'search/search_results.html', {'book_details': book_details})
+    tzomet_book_details = request.session.get('tzomet_results') ##1
+    stimazky_book_details = request.session.get('stimazky_results')
+    return render(request, 'search/search_results.html', {'tzomet_book_details': tzomet_book_details, 'stimazky_book_details': stimazky_book_details})
 
 
 
